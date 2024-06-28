e8f260427b3ac3934e5cc223e362b57915fcfc79
Gabriel-Villalba <105971696+Gabriel-Villalba@users.noreply.github.com>
2024-06-27 21:29:39 -0300

    mercado pago

--- src/components/NavBar/navBar.jsx
+++ src/components/NavBar/navBar.jsx
@@ -1,57 +1,13 @@
 import { Link } from 'react-router-dom';
-//import { useState } from 'react';
+import { useSelector } from 'react-redux';
 import LogoutButton from '../loginAuht0/Logout/Logout';
-
-
-import  { useState } from 'react';
-
 export default function NavBar() {
-    const [menuOpen, setMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
-
-    return (
-        <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#FF6F61' }}>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded={menuOpen} aria-label="Toggle navigation" onClick={toggleMenu}>
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className={`collapse navbar-collapse ${menuOpen ? 'show' : ''}`} id="navbarNav">
-                <ul className="navbar-nav" id="menuItems">
-                <li className="nav-item">
-                        <Link className="nav-link" to="/home">HOME</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/products">PRODUCTS</Link>
-                    </li>
-                    {/* <li className="nav-item">
-                        <Link className="nav-link" to="/form">ADMIN</Link>
-                    </li> */}
-                    <div className='Logout'>  <LogoutButton/></div>
-                </ul>
-            </div>
-        </nav>
-    );
-}
-
-
-
-
-
-
-
-
-/*export default function NavBar() {
-
-    const [menuOpen, setMenuOpen] = useState(false);
-    const toggleMenu = () => {
-        setMenuOpen(!menuOpen);
-    };
+    const isAdmin = useSelector(state => state.userAdmin); 
     return (
         <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#FF6F61' }}>
             
-            <button  onClick={toggleMenu} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation" >
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
@@ -62,14 +18,17 @@ export default function NavBar() {
                     <li className="nav-item">
                         <Link className="nav-link" to="/products">PRODUCTS</Link>
                     </li>
-                    {/* <li className="nav-item">
-                        <Link className="nav-link" to="/form">ADMIN</Link>
-                    </li> *
-                    <div className='Logout'>  <LogoutButton/></div>
+                    {isAdmin && (
+                    <li className="nav-item">
+                        <Link className="nav-link" to="/admin">ADMIN</Link>
+                    </li>
+
+                    )}
+                    <div className='botonLogOut'>  <LogoutButton/></div>
                 
             
                 </ul>
             </div>
         </nav>
     );
-}*/
+}
--- src/components/Order/Order22.jsx
+++ src/components/Order/Order22.jsx
@@ -1,72 +1,106 @@
-import  {useRef} from 'react';
-import { useSelector} from 'react-redux';
-import { Table } from 'react-bootstrap';
-import {useReactToPrint} from "react-to-print"
+import { useRef } from "react";
+import { useSelector } from "react-redux";
+import { Table } from "react-bootstrap";
+import { useReactToPrint } from "react-to-print";
+import { initMercadoPago, Wallet } from "@mercadopago/sdk-react";
+import axios from "axios";
+import { CREATE_PREFERENCE_Url } from "../../Redux/URLs/URLs";
+import { useState } from "react";
 
+function Orders() {
+  const actions = useSelector((state) => state.cart);
+  const [preferenceId, setPreferenceId] = useState(null);
 
+  const totalPrice =
+    actions.length > 0 &&
+    actions.reduce((total, item) => {
+      const price = parseFloat(item.Precio) || 0;
+      return total + price * item.amount;
+    }, 0);
 
-
-
-
-function Orders () {
-
-  const componentPDF= useRef()
-  const actions = useSelector(state => state.cart);
-  console.log(actions)
- 
-  const generatePDF = useReactToPrint({
-    content: ()=>componentPDF.current,
-    documentTitle: "Factura BellaMuse",
-    onAfterPrint: ()=>alert("Data save in PDF")
-
+  //*******Mercado Pago****** */
+  initMercadoPago("APP_USR-f30e2368-a4b5-4e70-be3e-da3cedb0b7b1", {
+    locale: "es-AR",
   });
-  let summary = 0
+console.log(totalPrice)
+  const createPreference = async () => {
+    try {
+      const response = await axios.post(CREATE_PREFERENCE_Url, {
+        title: "productos de indumentaria",
+        quantity: 1,
+        unit_price: totalPrice,
+      });
+      const { id } = response.data;
+      return id;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const componentPDF = useRef();
+ 
+  console.log(actions);
+
+  const generatePDF = useReactToPrint({
+    content: () => componentPDF.current,
+    documentTitle: "Factura BellaMuse",
+    onAfterPrint: () => alert("Data save in PDF"),
+  });
+  let summary = 0;
   actions.map((t) => {
-    return summary = summary + parseInt(t.total);
-  })
+    return (summary = summary + parseInt(t.total));
+  });
+
+  const handleBuy = async () => {
+    const id = await createPreference();
+    id ? setPreferenceId(id) : alert("Error");
+  };
 
   //  const handlePurchase = () => {
   //       alert('Pasarela de pagos del Sr Gabriel');
   //   };
-  const totalPrice = actions.length > 0 && actions.reduce((total, item) => {
-    const price = parseFloat(item.Precio) || 0;
-     return total + price * item.amount;
- }, 0);
 
   return (
-    <div className='container'>
-      
-        <div className='col-sm-12 col-md-12 col-lg-12 my-3' style={{ paddingTop: '20px' }}>
-           <div ref={componentPDF} style={{width:'100%'}}>
-              <h1>ORDEN DE COMPRA</h1>
-            <Table striped bordered hover> {/* Utilizar componente de tabla de Bootstrap */}
-              <thead>
-                <tr>
-                  <th>Producto</th>
-                  <th>Precio</th>
-                  <th>Cantidad</th>
-                  <th>Total</th>
+    <div className="container">
+      <div
+        className="col-sm-12 col-md-12 col-lg-12 my-3"
+        style={{ paddingTop: "20px" }}
+      >
+        <div ref={componentPDF} style={{ width: "100%" }}>
+          <h1>ORDEN DE COMPRA</h1>
+          <Table striped bordered hover>
+            {" "}
+            {/* Utilizar componente de tabla de Bootstrap */}
+            <thead>
+              <tr>
+                <th>Producto</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {actions.map((action) => (
+                <tr key={action.id}>
+                  <td>{action.Nombre}</td>
+                  <td>{action.Precio}</td>
+                  <td>{action.amount}</td>
+                  <td>{action.Precio * action.amount}</td>
                 </tr>
-              </thead>
-              <tbody>
-                {actions.map(action => (
-                  <tr key={action.id}>
-                    <td>{action.Nombre}</td>
-                    <td>{action.Precio}</td>
-                    <td>{action.amount}</td>
-                    <td>{ action.Precio * action.amount  }</td>
-                  </tr>
-                ))}
-                
-              </tbody>
-              
-            </Table>
-            <span> Total a pagar: {totalPrice} USD  </span>
-           </div>
+              ))}
+            </tbody>
+          </Table>
+          <span> Total a pagar: {totalPrice} USD </span>
         </div>
-        <button onClick={generatePDF}>PDF</button>
-        <button >Pagar</button>
-    
+      </div>
+      <button onClick={generatePDF}>PDF</button>
+      <button onClick={handleBuy}>Pagar</button>
+      {preferenceId && (
+        <Wallet
+          initialization={{ preferenceId: preferenceId }}
+          customization={{ texts: { valueProp: "smart_option" } }}
+        />
+      )}
     </div>
   );
 }
